81162c23724fe2eca8cc99b76008dff292c8f52a
Vishal Patil <vishalpatil.dev2@gmail.com>
2025-10-16 11:43:13 +0530

    Validation and Global Exception handler

--- src/main/java/com/koorier/warehouse/exception/GlobalExceptionHandler.java
+++ src/main/java/com/koorier/warehouse/exception/GlobalExceptionHandler.java
@@ -1,6 +1,24 @@
 package com.koorier.warehouse.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+	@ExceptionHandler
+	public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+		Map<String, String> errs = new HashMap<>();
+		errs = e.getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errs);
+	}
 
 }
